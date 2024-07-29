5866cb86ae8acaef1c4047ca97773139edc50fdb
travisnova <travistong@nova.com.hk>
2024-07-29 22:37:57 +0800

    split modules, add sorting logic, add tests

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
-// #region tmdb
-const popularMoviesUrl =
-  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTkwNDIzNDY3ODZhOTIxY2E3MzJmZmM0YzAxYTIxZiIsIm5iZiI6MTcyMTgwNzc3MC40NTAyNzcsInN1YiI6IjY2YTBhN2VhZjdhMTE0YTA4M2UwZDRhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SQjgB9waWCim1CPQFSbrHehebcAcr4uDQV8iIYgIrps",
-  },
-};
+import { sortMovies } from "./lib/sort-movies.js";
+import { getTMDBImageUrl } from "./lib/tmdb-api/api.js";
+import { formatNumber } from "./lib/formatting.js";
 
-function getTMDBImageUrl(path, size = "w500") {
-  return `https://image.tmdb.org/t/p/${size}${path}`;
-}
-// #endregion
+// make available to the global scope
+window.getTMDBImageUrl = getTMDBImageUrl;
+window.formatNumber = formatNumber;
 
-// #region formatting
-function formatNumber(value) {
-  return Math.round(value).toLocaleString("en-US");
-}
-// #endregion
-
-// #region data
 document.addEventListener("alpine:init", () => {
   Alpine.data("movies", () => ({
     movies: [],
+    sort: "default",
+    get sortedMovies() {
+      return sortMovies(this.movies, this.sort);
+    },
+    toggleSort() {
+      this.sort = this.sort === "popularity" ? "default" : "popularity";
+    },
     async init() {
       // fetch(popularMoviesUrl, options)
-      const response = await fetch("./sample_response.json").then((res) =>
-        res.json()
+      const response = await fetch("./lib/tmdb-api/sample_response.json").then(
+        (res) => res.json()
       );
       this.movies = response.results;
     },
   }));
 });
-// #endregion
