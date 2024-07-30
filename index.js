f9f36d922997cf197c432562bf6c94af9dd898b7
travisnova <travistong@nova.com.hk>
2024-07-30 23:08:04 +0800

    add animate cross movie backdrops

--- index.js
+++ index.js
@@ -10,15 +10,28 @@ document.addEventListener("alpine:init", () => {
   Alpine.data("movies", () => ({
     movies: [],
     sort: "default",
+    bannerIndex: 0,
+    get banner() {
+      if (!this.movies.length) return "";
+      return getTMDBImageUrl(
+        this.movies[this.bannerIndex].backdrop_path,
+        "original"
+      );
+    },
     get sortedMovies() {
       return sortMovies(this.movies, this.sort);
     },
     toggleSort() {
       this.sort = this.sort === "popularity" ? "default" : "popularity";
     },
+    nextBanner() {
+      console.log("next banner", this.bannerIndex);
+      this.bannerIndex = (this.bannerIndex + 1) % 5;
+    },
     async init() {
       const response = await fetchPopularMovies();
       this.movies = response.results;
+      setInterval(this.nextBanner.bind(this), 5000);
     },
   }));
   Alpine.store("mobileMenu", {
